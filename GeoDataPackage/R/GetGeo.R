e1afef9af6063d33b7a8440e0a99657a158fcb59
JanMultmeier <Jan.Multmeier@googlemail.com>
2015-05-13 22:35:12 +0200

    Added datasciencetoolkit.org as possible source

--- GeoDataPackage/R/GetGeo.R
+++ GeoDataPackage/R/GetGeo.R
@@ -1,18 +1,28 @@
 #' Geocodes a geographical location and returns it latitude and longitude
 #' 
 #' @param addr character vector containing the location to geocode
+#' @param source either "google" (default) to retriece data from the google maps API, or "dstk" for datasciencetoolkit.org
+#' @param country needed if source="dstk" to avoid empty results. Defaults to "Germany".
 #' @return a data.frame with lat and lon as columns
-#' @keywords Google API geodata geocode location
+#' @keywords Google dstk API geodata geocode location
 #' @export
 #' @examples
 #' getGeo("Weststr. 88, 33615 Bielefeld")
 #' getGeo("Berlin, Alexanderplatz")
 
-getGeo <- function(addr){
+getGeo <- function(source="google",addr,country="Germany"){
   require(rjson)
   results <- data.frame("lat"=numeric(length=length(addr)),"lon"=numeric(length=length(addr)))
-  for (i in seq_along(addr)) {
-    url <- paste0("http://maps.googleapis.com/maps/api/geocode/json?address=",gsub(" ","+",addr[i]),"&sensor=false")
+  if (source=="google") {
+    for (i in seq_along(addr)) {
+      url <- paste0("http://maps.googleapis.com/maps/api/geocode/json?address=",gsub(" ","+",addr[i]),"&sensor=false")
+    }
+  }
+  if (source=="dstk") {
+    for (i in seq_along(addr)) {
+      url <- paste0("http://www.datasciencetoolkit.org/maps/api/geocode/json?sensor=false&address=",gsub(" ","+",addr[i]),",+",country[i])
+    }
+  }
     data <- fromJSON(file=url)
     if (data$status=="ZERO_RESULTS") {
       warning("Keine Ergebnisse gefunden!")
@@ -28,6 +38,5 @@ getGeo <- function(addr){
       results$lat[i] <- NA
       results$lon[i] <- NA
     }
-  }
   return(results)
 }
