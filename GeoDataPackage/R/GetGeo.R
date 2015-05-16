6cb837ad7335ecde62ff9a72896e4af801090e17
JanMultmeier <Jan.Multmeier@googlemail.com>
2015-05-16 15:30:35 +0200

    Added support for ArcGis gecoding API

--- GeoDataPackage/R/GetGeo.R
+++ GeoDataPackage/R/GetGeo.R
@@ -1,8 +1,8 @@
 #' Geocodes a geographical location and returns it latitude and longitude
 #' 
 #' @param addr character vector containing the location to geocode
-#' @param source either "google" (default) to retriece data from the google maps API, or "dstk" for datasciencetoolkit.org
-#' @param local boolean, if TRUE, retrieves values from local machine, FALSE connects to datasciencetoolkit.org (default)
+#' @param source either "google" (default) to retriece data from the google maps API, "dstk" for datasciencetoolkit.org, or "arcgis" to use ArcGis by Esri
+#' @param local boolean. If TRUE, retrieves values from local machine when using dstk as a source, FALSE connects to datasciencetoolkit.org (default)
 #' @param country needed if source="dstk" to avoid empty results. Defaults to "Germany".
 #' @return a data.frame with columns lat and lon
 #' @keywords Google dstk API geodata geocode location
@@ -13,11 +13,12 @@
 #' getGeo("4308 Lookout Rd, 23455 Virginia Beach, VA",source="dstk",country="USA")
 
 getGeo <- function(addr,source="google",localhost=FALSE,country="Germany") {
-  if (source!="google"&source!="dstk") {stop("Unbekanntes 'source'-Argument: google oder dstk")}
+  if (source!="google"&source!="dstk"&source!="arcgis") {stop("Unbekanntes 'source'-Argument: google, dstk, oder arcgis")}
   require(rjson)
   cntry <- rep(country, times=length(addr))
   results <- data.frame("lat"=numeric(length=length(addr)),"lon"=numeric(length=length(addr)))
     for (i in seq_along(addr)) {
+      if (source %in% c("google","dstk")){
         if (source=="google") {
           url <- paste0("http://maps.googleapis.com/maps/api/geocode/json?address=",gsub(" ","+",addr[i]),"&sensor=false")
         }
@@ -30,7 +31,6 @@ getGeo <- function(addr,source="google",localhost=FALSE,country="Germany") {
           warning("Keine Ergebnisse gefunden!")
         }
         if (data$status=="OVER_QUERY_LIMIT") {
-          #cont <- FALSE
           stop("Abfragelimit erreicht")
         }
         if (data$status=="OK") {
@@ -41,6 +41,13 @@ getGeo <- function(addr,source="google",localhost=FALSE,country="Germany") {
           results$lat[i] <- NA
           results$lon[i] <- NA
         }
+      }
+    if (source=="arcgis") {
+      url <- paste0("http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/find?text=",gsub(" ","+",addr[i]),"&f=json")
+      data <- fromJSON(file=url)
+      results$lat[i] <- data$locations[[1]]$feature$geometry$y
+      results$lon[i] <- data$locations[[1]]$feature$geometry$x
+    }
     }
 return(results)
 }
